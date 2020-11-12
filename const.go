266b9cacacf2ed713fca35e34d25368f362bc963
Dipankar Saha <>
2020-11-12 12:27:16 +0600

    constants updated

--- const.go
+++ const.go
@@ -1,19 +1,27 @@
 package collpay_go_sdk
 
 const (
+	//env types
 	ENV_PRODUCTION = 1
 	ENV_SANDBOX = 2
+
+	//versions
 	V1 = "v1"
+
+	//payment transaction statuses
 	TRANSACTION_PROCESSING = "Processing"
-	TRANSACTION_CONFIRMED = "Confirmed"
 	TRANSACTION_NOTIFIED = "Notified"
-	TRANSACTION_COMPLETED = "Completed"
+	TRANSACTION_EXPIRED = "Expired"
+	TRANSACTION_CONFIRMED = "Confirmed"
+	/*TRANSACTION_COMPLETED = "Completed"
 	TRANSACTION_FAILED = "Failed"
 	TRANSACTION_REJECTED = "Rejected"
-	TRANSACTION_EXPIRED = "Expired"
 	TRANSACTION_BLOCKED = "Blocked"
 	TRANSACTION_REFUNDED = "Refunded"
-	TRANSACTION_VOIDED = "Voided"
+	TRANSACTION_VOIDED = "Voided"*/
+
+	//ipn or webhook events
+	PAYMENT_EVENT = "payment"
 )
 
 var (
